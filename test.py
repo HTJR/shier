c8ae8fc5aab00e6cbf5e8c89fa851703c0a0f800
HTJR <hrashikeshtiwari1234@gmail.com>
2021-01-21 00:27:44 +0530

    req

--- test.py
+++ test.py
@@ -22,24 +22,23 @@ st_lottie(lottie_json)
 
 
 
+# Specify canvas parameters in application
 stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
 stroke_color = st.sidebar.color_picker("Stroke color hex: ")
 bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
-bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
+#bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
 drawing_mode = st.sidebar.selectbox(
     "Drawing tool:", ("freedraw", "line", "rect", "circle", "transform")
 )
-realtime_update = st.sidebar.checkbox("Update in realtime", True)
-
 # Create a canvas component
 canvas_result = st_canvas(
-    fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
+    fill_color="rgb(255, 165, 0)",  # Fixed fill color with some opacity
     stroke_width=stroke_width,
     stroke_color=stroke_color,
-    background_color="" if bg_image else bg_color,
-    background_image=Image.open(bg_image) if bg_image else None,
-    update_streamlit=realtime_update,
-    height=150,
+    background_color= bg_color,
+    background_image=None,
+    update_streamlit=True,
+    height=550,
     drawing_mode=drawing_mode,
     key="canvas",
 )
