7c44ac0172fc90a625c4353f52de5620e94b961a
HTJR <hrashikeshtiwari1234@gmail.com>
2021-01-21 00:43:19 +0530

    req

--- test.py
+++ test.py
@@ -31,15 +31,7 @@ drawing_mode = st.sidebar.selectbox(
     "Drawing tool:", ("freedraw", "line", "rect", "circle", "transform")
 )
 # Create a canvas component
-canvas_result = st_canvas(
-    stroke_width=stroke_width,
-    stroke_color=stroke_color,
-    background_color= bg_color,
-    background_image=None,
-    update_streamlit=True,
-    height=550,
-    drawing_mode=drawing_mode,
-    key="canvas")
+canvas_result = st_canvas(stroke_width=stroke_width,stroke_color=stroke_color,background_color= bg_color,background_image=None,update_streamlit=True,height=550,drawing_mode=drawing_mode,key="canvas")
 
 # Do something interesting with the image data and paths
 im=canvas_result.image_data
