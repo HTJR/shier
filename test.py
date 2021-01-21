6fe542c962a0a6c9900fe054dbf3c35f77292c53
HTJR <hrashikeshtiwari1234@gmail.com>
2021-01-21 12:09:48 +0530

    pickle

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ import requests
 from PIL import Image
 st.title("Le Art")
 st.write("MoDeRn ArT ")
-"""
+
 def load_lottieurl(url: str):
     r = requests.get(url)
     if r.status_code != 200:
@@ -19,7 +19,6 @@ def load_lottieurl(url: str):
 lottie_url = "https://assets6.lottiefiles.com/private_files/lf30_nj9klqqz.json"
 lottie_json = load_lottieurl(lottie_url)
 st_lottie(lottie_json)
-"""
 
 
 # Specify canvas parameters in application
